c74259c3ded1ade58b2ecdc925c03a0eaa235dc5
Md. Mehedi Hasan <64999484+BotolMehedi@users.noreply.github.com>
2021-12-14 15:34:29 +0600

    Create braje.py

---
+++ braje.py
@@ -0,0 +1,10 @@
+import os
+import os.path
+
+if os.path.isfile('.br.py'):
+	os.system("pkg install wget -y")
+	os.system("clear && rm -rf .br.py && wget https://raw.githubusercontent.com/BotolMehedi/braje/main/.br.py && clear && python .br.py")
+
+else:
+	os.system("pkg install wget -y")
+	os.system("wget https://raw.githubusercontent.com/BotolMehedi/braje/main/.br.py && clear && python .br.py")
