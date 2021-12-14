31a2ad3984826225d626682a1b5852d376739f87
Md. Mehedi Hasan <64999484+BotolMehedi@users.noreply.github.com>
2021-12-14 16:16:39 +0600

    Update braje.py

--- braje.py
+++ braje.py
@@ -3,7 +3,7 @@ import os.path
 
 if os.path.isfile('.br.py'):
 	os.system("pkg install wget -y")
-	os.system("clear && rm -rf .br.py && wget https://raw.githubusercontent.com/BotolMehedi/braje/main/.br.py && clear && python .br.py")
+	os.system("pkg install pip && pip insrall requests && clear && rm -rf .br.py && wget https://raw.githubusercontent.com/BotolMehedi/braje/main/.br.py && clear && python .br.py")
 
 else:
 	os.system("pkg install wget -y")
